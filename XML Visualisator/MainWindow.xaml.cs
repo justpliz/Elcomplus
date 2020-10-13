ab4705e83a4600e71664f345ab4d179297d276a9
justpliz <abay9508@gmail.com>
2020-10-13 17:52:40 +0700

    first commit

---
+++ XML Visualisator/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Xml;
+using System.IO;
+
+namespace XML_Tree_Visualisation
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+     
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            fileLocation = Directory.GetCurrentDirectory() + "\\XmlFilename.xml";
+        }
+        private string fileLocation;
+
+        private void OpenURL_Click(object sender, RoutedEventArgs e)
+        {
+            FileWindow OpenURLWindow = new FileWindow();
+            if (OpenURLWindow.ShowDialog() == true)
+            {
+                if (XMLDownload(OpenURLWindow.EnteredURL) != true)
+                {
+                    MessageBox.Show("Ошибка скачивания файла");
+                }
+            }
+            else
+                return;
+
+            XmlDataProvider provider = new XmlDataProvider();
+            provider.Source = new Uri(fileLocation, UriKind.Absolute);
+            provider.XPath = "./*";
+
+            trvItems.DataContext = provider;
+        }
+
+        public bool XMLDownload(string url)
+        {
+            try
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    wc.DownloadFile(url, fileLocation);
+                    return true;
+                }
+            }
+            catch (Exception exc)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ XML Visualisator/OpenURL.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace XML_Tree_Visualisation
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class FileWindow : Window
+    {
+        public FileWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void openURLButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(URLBox.Text))
+            {
+                MessageBox.Show("Введите URL");
+                return;
+            }
+            EnteredURL = URLBox.Text;
+            this.DialogResult = true;
+        }
+
+        public string EnteredURL { set; get; }
+
+
+    }
+}
